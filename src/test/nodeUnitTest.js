ebd4c9b6a491969e211d1d0c07a0e1b5d05dc308
ken <ken.fuguo@gmail.com>
2016-03-24 11:22:15 +0000

    nodeunit extra module for test

---
+++ src/test/nodeUnitTest.js
@@ -0,0 +1,28 @@
+/**
+ * Created by ibm on 24/03/2016.
+ */
+// npm insall nodeunit
+var test = require('nodeunit');
+
+module.exports = {
+    'nodeUnitTest' : {
+        'equal' : function (test) {
+            test.equal(3, '3', '3 euqals "3"');
+            test.done();
+        },
+        'strictEqual': function (test) {
+            test.strictEqual('3', 3, '3 and "3" are not strictly equal');
+            test.done();
+        },
+        "notEqual": function (test) {
+            test.notEqual(3,'three', '3 does not equal to "three"');
+            test.done();
+        },
+        'ok':function (test) {
+            test.ok(false, "not true");
+            test.done(); // just to tell the test is done
+        }
+    }
+}
+
+// run: nodeunit nodeUnitTest.js
