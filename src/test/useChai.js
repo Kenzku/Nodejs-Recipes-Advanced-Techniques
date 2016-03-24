e9f32f92d29057b239f2c2ec1dce9b4bb896ab2d
ken <ken.fuguo@gmail.com>
2016-03-24 11:39:41 +0000

    use chai

---
+++ src/test/useChai.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ibm on 24/03/2016.
+ */
+// npm install chai
+var chai = require('chai');
+
+var assert = chai.assert; // chai's assert module
+
+assert.equal(three, '3', '3 == "3"');
+
+assert.strictEqual('3', three.toString(), '3 and "3" are not strict euqual');
+
+assert.ok(true, 'is truthy');
+
+// Chai is mainly used for Behaviour testing
+var expect = chai.expect;
+
+expect(three).to.be.a('number');
+
+var should = chai.should();
+
+three.should.be.a('number');
+
+three.should.be.a('String'); // this should fail
+
+// run: node useChai.js
--- src/test/useNodeunit.js
+++ src/test/useNodeunit.js
@@ -25,4 +25,4 @@ module.exports = {
     }
 }
 
-// run: nodeunit nodeUnitTest.js
+// run: nodeunit useNodeunit.js
