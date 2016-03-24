f422b29eb970517f1fe3d30b9e6961d80b784c25
ken <ken.fuguo@gmail.com>
2016-03-24 11:30:08 +0000

    use mocha

---
+++ src/test/useMocha.js
@@ -0,0 +1,28 @@
+/**
+ * Created by ibm on 24/03/2016.
+ */
+// npm install mocha
+
+var assert = require('assert');
+
+var three = 3;
+
+describe('useMocha', function () { // suite
+    describe('#equal', function () { // describe one test
+        it('should return true that 3 equals 3', function () {
+            assert.equal(three, '3', '3 equals "3"');
+        });
+    });
+    describe('#strictEqual', function () { // describe another test
+        it('"3" only strictly equals 3.toString()', function () {
+            assert.strictEqual('3', three.toString(), '3 and "3" are not strictly euqal');
+        });
+    });
+    describe('#ok', function () {
+        it('should return that false is not truthy', function () {
+            assert.ok(false, 'not truthy'); // this will fail
+        });
+    });
+});
+
+// mocha useMocha.js
