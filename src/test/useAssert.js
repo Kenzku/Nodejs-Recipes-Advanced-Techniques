2ff232a78c0b4fa0cedaf57fcbda4e201604f552
ken <ken.fuguo@gmail.com>
2016-03-24 11:08:13 +0000

    assert

---
+++ src/test/useAssert.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ibm on 24/03/2016.
+ */
+var assert = require('assert');
+
+var three = 3;
+
+assert.equal(three, '3', '3 equals 3'); // should not return anything
+
+assert.strictEqual(three.toString(), '3', '3 and "3" are not strictly equal');
+
+assert.notEqual(three, 'three', 'the integer 3 is not equal to three');
+
+assert.ok(true, 'truthy');
+
+//assert.ok(false, 'not true'); // will throw error
